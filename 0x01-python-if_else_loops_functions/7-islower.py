f0b1b746cca7f57646358f5d590e1628ac3c50f8
XELELO <yamkelaxelelo@gmail.com>
2022-08-24 00:44:40 -0700

    first commit

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Yamkela Xelelo
+
+def islower(c):
+        """Function checks for lowercase characters."""
+            if ord(c) >= 97 and ord(c) <= 122:
+                        return True
+                        else:
+                                    return False
